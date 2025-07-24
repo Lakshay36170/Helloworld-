1e1410637b5e990151f75bb02ca60ef8ea32d2f9
Lakshay Verma <lakshayverma36170@gmail.com>
2025-07-24 11:40:57 +0530

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,9 @@
+def main():
+    """
+    Entry point for the Hello World program.
+    Prints a greeting message to standard output.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
